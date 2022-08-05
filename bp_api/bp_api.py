b7f2bd1e859b2ae0bedbff00d89505e8b5e96957
mkNd <mknord1565@gmail.com>
2022-08-06 00:31:57 +0300

    continue changing utils to DAO

--- bp_api/bp_api.py
+++ bp_api/bp_api.py
@@ -1,23 +1,24 @@
 from flask import Blueprint, jsonify
-from utils import get_all_posts, get_post_by_pk
+from posts_dao import PostsDAO
 from logger import get_and_set_logger
 import logging
 
 get_and_set_logger()
 logger = logging.getLogger('basic')
 
+posts_dao = PostsDAO()
 bp_api = Blueprint('bp_api', __name__)
 
 
 @bp_api.route('/posts/')
 def page_posts_as_api():
     logger.info('request to /api/posts')
-    posts = get_all_posts()
+    posts = posts_dao.get_all_posts()
     return jsonify(posts)
 
 
 @bp_api.route('/posts/<int:post_id>')
 def page_post_by_id_as_api(post_id):
     logger.info(f'request to /api/posts/{post_id}')
-    post = get_post_by_pk(post_id)
+    post = posts_dao.get_post_by_pk(post_id)
     return jsonify(post)
