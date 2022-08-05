345615b28e9cf1b49a59bfcd758131613130436e
mkNd <mknord1565@gmail.com>
2022-08-06 00:01:31 +0300

    continue changing utils to DAO

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from utils_with_tags import change_words_with_tag_to_link_in_content, get_first_tag_word, get_posts_by_tag_word
 from utills_with_bookmark import get_posts_from_bookmarks
 from flask import Flask, render_template, request
 from posts_dao import PostsDAO
 from comments_dao import CommentsDAO
+from tags_dao import TagsDAO
 
 from bp_bookmarks.bp_bookmarks import bp_bookmarks
 from bp_api.bp_api import bp_api
@@ -10,6 +10,7 @@ from bp_error.bp_error import bp_error
 
 posts_dao = PostsDAO()
 comments_dao = CommentsDAO()
+tags_dao = TagsDAO()
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -22,7 +23,6 @@ app.register_blueprint(bp_api, url_prefix='/api/')
 
 @app.route('/')
 def page_lent():
-    # all_posts = get_all_posts()
     all_posts = posts_dao.get_all_posts()
     bookmarks = get_posts_from_bookmarks()
     return render_template('index.html', all_posts=all_posts, bookmarks=bookmarks)
@@ -30,10 +30,8 @@ def page_lent():
 
 @app.route('/post/<int:pk>')
 def page_post_by_id(pk):
-    # post = get_post_by_pk(pk)
     post = posts_dao.get_post_by_pk(pk)
-    post['content'] = change_words_with_tag_to_link_in_content(post['content'])
-    # comments = get_comments_by_post_id(pk)
+    post['content'] = tags_dao.change_tag_words_to_link_in_content(post['content'])
     comments = comments_dao.get_comments_by_post_id(pk)
     return render_template('post.html', post=post, comments=comments)
 
@@ -41,30 +39,28 @@ def page_post_by_id(pk):
 @app.route('/search/')
 def page_with_posts_by_query():
     s = request.values.get('s')
-    # posts = search_for_posts(s)
     posts = posts_dao.search_for_posts(s)
     for post in posts:
-        post['tag'] = get_first_tag_word(post['content'])
-        post['content'] = change_words_with_tag_to_link_in_content(post['content'])
+        post['tag'] = tags_dao.get_first_tag_word(post['content'])
+        post['content'] = tags_dao.change_tag_words_to_link_in_content(post['content'])
     return render_template('search.html', posts=posts)
 
 
 @app.route('/users/<user_name>')
 def page_with_posts_by_user(user_name):
-    # posts = get_posts_by_user(user_name)
     posts = posts_dao.get_posts_by_user(user_name)
     for post in posts:
-        post['tag'] = get_first_tag_word(post['content'])
-        post['content'] = change_words_with_tag_to_link_in_content(post['content'])
+        post['tag'] = tags_dao.get_first_tag_word(post['content'])
+        post['content'] = tags_dao.change_tag_words_to_link_in_content(post['content'])
     return render_template('user_feed.html', posts=posts, user_name=user_name)
 
 
 @app.route('/tag/<tag_word>')
 def page_with_posts_by_tag(tag_word):
-    posts = get_posts_by_tag_word(tag_word)
+    posts = tags_dao.get_posts_by_tag_word(tag_word)
     for post in posts:
-        post['tag'] = get_first_tag_word(post['content'])
-        post['content'] = change_words_with_tag_to_link_in_content(post['content'])
+        post['tag'] = tags_dao.get_first_tag_word(post['content'])
+        post['content'] = tags_dao.change_tag_words_to_link_in_content(post['content'])
     return render_template('tag.html', posts=posts, tag_word=tag_word)
 
 
---
+++ tags_dao.py
@@ -0,0 +1,58 @@
+from utils import get_all_posts
+
+
+class TagsDAO:
+
+    def is_post_with_tag(self, content: str) -> bool:
+        """Проверяет пост на наличие тэга #"""
+        if '#' in content:
+            return True
+
+    def clean_tag_ward(self, tag_word: str) -> str:
+        """Очищает слово с тэгом от символов в конце"""
+        symbols_to_remove = ('.', ',', '!', '?')
+        if tag_word[-1] in symbols_to_remove:
+            tag_word = tag_word[:-1]
+        return tag_word
+
+    def get_first_tag_word(self, content: str) -> str:
+        """Возвращает первое слово с тэгом из текста"""
+        firs_tag_word = ''
+        for word in content.split(' '):
+            if len(word) > 1 and word[0] == '#':
+                firs_tag_word = self.clean_tag_ward(word)
+                return firs_tag_word
+        return firs_tag_word
+
+    def get_list_with_tag_words(self, content: str) -> list:
+        """Возвращает список слов, начинающихся с тэга #"""
+        words_with_tag = []
+        for word in content.split(' '):
+            if len(word) > 1 and word[0] == '#':
+                word = self.clean_tag_ward(word)
+                words_with_tag.append(word)
+        return words_with_tag
+
+    def change_tag_words_to_link_in_content(self, content: str) -> str:
+        """Заменяет слова в тексте на ссылки"""
+        if self.is_post_with_tag(content):
+            words_with_tag = self.get_list_with_tag_words(content)
+            for word in words_with_tag:
+                content = content.replace(word, f'<a href="/tag/{word[1:]}">{word}</a>')
+        return content
+
+    def change_tag_words_to_link_in_all_posts_and_add_first_tag_word(self, posts: list[dict]) -> list[dict]:
+        """Заменяет слова во всех постах на ссылки"""
+        for post in posts:
+            post['tag'] = self.get_first_tag_word(post['content'])
+            post[post['content']] = self.change_tag_words_to_link_in_content(post['content'])
+        return posts
+
+    def get_posts_by_tag_word(self, tag_word: str) -> list[dict]:
+        """Возвращает посты с тэгом"""
+        all_posts = get_all_posts()
+        wanted_posts = []
+        for post in all_posts:
+            if '#' + tag_word in post['content']:
+                wanted_posts.append(post)
+        return wanted_posts
